8af28cec2289c0b0f90aab09a628455378ae1e13
ubaid <ubaidshowkat91@gmail.com>
2019-02-13 14:55:56 +1100

    Document format

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,22 @@
-import { NgModule }             from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { PersonalComponent }    from './personal/personal.component';
-import { WorkComponent }        from './work/work.component';
-
+import { PersonalComponent } from "./personal/personal.component";
+import { WorkComponent } from "./work/work.component";
 
 export const appRoutes: Routes = [
-    // 1st Route
-    { path: 'personal',  component: PersonalComponent },
-    // 2nd Route
-    { path: 'work',  component: WorkComponent },
-    // 3rd Route
-    { path: '',   redirectTo: '/personal', pathMatch: 'full' },
-    // 4th Route
-    { path: '**', component: PersonalComponent }
+  // 1st Route
+  { path: "personal", component: PersonalComponent },
+  // 2nd Route
+  { path: "work", component: WorkComponent },
+  // 3rd Route
+  { path: "", redirectTo: "/personal", pathMatch: "full" },
+  // 4th Route
+  { path: "**", component: PersonalComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes, { useHash: true} )],
-  exports: [RouterModule],
+  imports: [RouterModule.forRoot(appRoutes, { useHash: true })],
+  exports: [RouterModule]
 })
-
 export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormDataService }   from './data/formData.service';
+import { Component, OnInit, Input } from "@angular/core";
+import { FormDataService } from "./data/formData.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: "app-root",
+  templateUrl: "./app.component.html"
   //styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'Plexus Legal';
+  title = "Plexus Legal";
 
   @Input() formData;
-    
-  constructor(private formDataService: FormDataService) {
-  }
+
+  constructor(private formDataService: FormDataService) {}
 
   ngOnInit() {
-      this.formData = this.formDataService.getFormData();
-      console.log(this.title + ' loaded!');
+    this.formData = this.formDataService.getFormData();
+    console.log(this.title + " loaded!");
   }
-
 }
