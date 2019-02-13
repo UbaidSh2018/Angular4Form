d67f2c5d1b8e8ca85f5c756e92fe1e71e95bae98
ubaid <ubaidshowkat91@gmail.com>
2019-02-13 13:37:17 +1100

    Creating application root component and its html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormDataService }   from './data/formData.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Plexus Legal';
+
+  @Input() formData;
+    
+  constructor(private formDataService: FormDataService) {
+  }
+
+  ngOnInit() {
+      this.formData = this.formDataService.getFormData();
+      console.log(this.title + ' loaded!');
+  }
+
 }
